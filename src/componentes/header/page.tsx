6968b56e5d235aab8efb46321715f6d3445dcee0
josehenriquedasilva <josehenrique60t@gmail.com>
2026-01-21 16:32:42 -0300

    Produtos São Bento

--- src/componentes/header/page.tsx
+++ src/componentes/header/page.tsx
@@ -70,7 +70,7 @@ export default function Header() {
       ></div>
       <nav
         className={`${
-          menu ? `traslate-x-120 block` : `translate-x-190 invisible`
+          menu ? `traslate-x-120 visible` : `translate-x-190 invisible`
         } absolute h-screen flex flex-col items-end px-5 py-8 gap-5 w-[85%] right-0 top-0 text-shadow-lg/10 transition-all duration-200 z-30 bg-[#4A148C] md:hidden`}
       >
         <button className="text-[#F17E21] cursor-pointer " onClick={toggleMenu}>
