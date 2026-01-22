fd60201b4cfa6f0a84234a8bb5de79bf18143919
josehenriquedasilva <josehenrique60t@gmail.com>
2026-01-22 14:43:56 -0300

    Produtos São Bento

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export const metadata: Metadata = {
     siteName: title,
     images: [
       {
-        url: `https://produtos-sao-bento.vercel.app/logoSB-2.jpg`,
+        url: `https://produtos-sao-bento.vercel.app/logoSB-2.png`,
         width: 1200,
         height: 630,
         alt: `${title} | Com Amor e Qualidade para Sua Mesa`,
@@ -84,7 +84,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: `${title} | Com Amor e Qualidade para Sua Mesa`,
     description: description,
-    images: [`https://produtos-sao-bento.vercel.app/logoSB-2.jpg`],
+    images: [`https://produtos-sao-bento.vercel.app/logoSB-2.png`],
   },
 };
 
